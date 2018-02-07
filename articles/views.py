5b560249e77ca65de06fb1a1c09b1584a384cc5e
jagans94 <jagans94@gmail.com>
2018-02-07 19:55:05 +0530

    added template tags

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 
 def articles(request):
-	return render(request,'articles/articles.html')
+	articles = Article.objects.all().order_by('date')
+	context = {
+		"articles" : articles
+	}
+	return render(request,'articles/articles.html',context)
