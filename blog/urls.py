ea41a4ebf75e22a4e612cbad9e3b9600b7c5dd2f
jagans94 <jagans94@gmail.com>
2018-02-08 18:12:26 +0530

    changed layout

--- blog/urls.py
+++ blog/urls.py
@@ -5,10 +5,12 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
 
+app_name = 'blog'
+
 urlpatterns = [ 
     path('admin/', admin.site.urls),	
     path('about/',views.about),
-    path('',views.home),
+    path('',views.home, name='home'),
     path('articles/',include('articles.urls')),
     ]
 
