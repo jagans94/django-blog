4960d8ac38005fa17c9865ab0c9f4801fb223bc4
jagans94 <jagans94@gmail.com>
2018-02-09 17:21:46 +0530

    added logout functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,7 @@ from .import views
 app_name = 'accounts'
 
 urlpatterns = [
-    re_path(r'^signup/$',views.signup,name='signup'),
-    re_path(r'^signin/$',views.signin,name='signin'),
+    re_path(r'^signup/$',views.signup_view,name='signup'),
+    re_path(r'^signin/$',views.signin_view,name='signin'),
+    re_path(r'^signout/$',views.signout_view,name='signout'),
     ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 # Create your views here.
-def signup(request):
+def signup_view(request):
 	if request.method == 'POST':
 		form = UserCreationForm(request.POST)
 		if form.is_valid():
 			form.save()
 			# log the user in
-			return redirect('home')
+			return redirect('accounts:signin')
 	else:
 		form = UserCreationForm()
 
@@ -16,12 +17,14 @@ def signup(request):
 		}
 	return render(request,'accounts/signup.html',context)
 
-def signin(request):
+def signin_view(request):
 	if request.method == 'POST':
 		form = AuthenticationForm(data=request.POST)
 		if form.is_valid():
 			# log the user in
-			return redirect('home')
+			user = form.get_user()
+			login(request,user)
+			return redirect('articles:list')
 	else:
 		form = AuthenticationForm()
 
@@ -29,3 +32,8 @@ def signin(request):
 		'form' : form
 		}
 	return render(request,'accounts/signin.html',context)
+
+def signout_view(request):
+	if request.method == 'POST':
+		logout(request)
+		return redirect('articles:list')
